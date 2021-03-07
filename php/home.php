eaa4509dbdc9739b1a72b2465a9a68fb42901f4a
mandy-lin <66708315+mandy-lin@users.noreply.github.com>
2021-03-07 18:08:04 +0800

    Delete php directory

--- php/home.php
+++
@@ -1,8 +0,0 @@
-<header>
-<button onclick="list()"  class='list'><i class="fa fa-bars" aria-hidden="true"></i></button>
-
-<h1>
-<img  src="../img/jj.png" class="image" title="健璟實業股份有限公司">
-健璟實業股份有限公司</h1>
-<button onclick="location.href='logout.php'" class='logout-btn' >登出</button>
-</header>
